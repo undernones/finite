da57d5e4c9def6531c1c0f8e2adc230500b37f20
undernones <sjward@gmail.com>
2012-10-19 07:54:25 -0700

    Split step() into multiple helper functions to help with profiling.

--- main.cpp
+++ main.cpp
@@ -8,68 +8,76 @@
 #include "MeshLoader.h"
 #include "Options.h"
 
+using Eigen::Matrix3d;
+
 namespace
 {
 
 const int SUCCESS = 0;
 const int ERROR = 1;
 
-std::vector<Eigen::Matrix3d> sDeformations;
-std::vector<Eigen::Matrix3d> sStrains;
-std::vector<Eigen::Matrix3d> sStresses;
+std::vector<Matrix3d> sDeformations;
+std::vector<Matrix3d> sStrains;
+std::vector<Matrix3d> sStresses;
 
 }
 
 // --------------------------------------------------------------------------
 
 void
-step(Mesh *mesh, double dt)
+computeDeformations(const std::vector<Tetrahedron>& tets,
+                    std::vector<Matrix3d>& deformations)
 {
-    assert(mesh != NULL);
-
-    std::vector<Tetrahedron>::const_iterator tet_it;
-    std::vector<Eigen::Matrix3d>::iterator def_it;
-    std::vector<Eigen::Matrix3d>::iterator strain_it;
-    std::vector<Eigen::Matrix3d>::iterator stress_it;
-
-    // Clear forces
-    for (Vertex& v : mesh->verts) {
-        v.f = Eigen::Vector3d(0, 0, 0);
-    }
-
-    // Deformation
-    tet_it = mesh->tets.begin();
-    def_it = sDeformations.begin();
-    for (; def_it != sDeformations.end(); ++tet_it, ++def_it) {
+    std::vector<Tetrahedron>::const_iterator tet_it = tets.begin();
+    std::vector<Matrix3d>::iterator def_it = deformations.begin();
+    for (; def_it != deformations.end(); ++tet_it, ++def_it) {
         *def_it = tet_it->X() * tet_it->Beta();
     }
+}
+// --------------------------------------------------------------------------
 
-    // Strain
-    def_it = sDeformations.begin();
-    strain_it = sStrains.begin();
-    for (; strain_it != sStrains.end(); ++def_it, ++strain_it) {
-        const Eigen::Matrix3d& F = *def_it;
-        *strain_it = 0.5 * (F + F.transpose()) - Eigen::Matrix3d::Identity();
+void
+computeStrains(const std::vector<Matrix3d>& deformations,
+               std::vector<Matrix3d>& strains)
+{
+    std::vector<Matrix3d>::const_iterator def_it = deformations.begin();
+    std::vector<Matrix3d>::iterator strain_it = strains.begin();
+    for (; strain_it != strains.end(); ++def_it, ++strain_it) {
+        const Matrix3d& F = *def_it;
+        // Linear Cauchy strain
+        *strain_it = 0.5 * (F + F.transpose()) - Matrix3d::Identity();
     }
+}
+// --------------------------------------------------------------------------
 
-    // Stress
-    def_it = sDeformations.begin();
-    strain_it = sStrains.begin();
-    stress_it = sStresses.begin();
+void
+computeStresses(const std::vector<Matrix3d>& deformations,
+                const std::vector<Matrix3d>& strains,
+                std::vector<Matrix3d>& stresses)
+{
+    std::vector<Matrix3d>::const_iterator def_it = deformations.begin();
+    std::vector<Matrix3d>::const_iterator strain_it = strains.begin();
+    std::vector<Matrix3d>::iterator stress_it = stresses.begin();
     for (; stress_it != sStresses.end(); ++strain_it, ++def_it, ++stress_it) {
-        const Eigen::Matrix3d& strain = *strain_it;
-        const Eigen::Matrix3d& F = *def_it;
+        const Matrix3d& strain = *strain_it;
+        const Matrix3d& F = *def_it;
 
         double d = Options::lambda() * strain.trace();
-        Eigen::Matrix3d diag;
+        Matrix3d diag;
         diag << d, 0, 0,
                 0, d, 0,
                 0, 0, d;
         *stress_it = F * (2 * Options::mu() * strain + diag);
     }
+}
+// --------------------------------------------------------------------------
 
-    // Internal Force
-    stress_it = sStresses.begin();
+void
+computeForces(const std::vector<Matrix3d>& stresses, Mesh *mesh)
+{
+    assert(mesh != NULL);
+
+    std::vector<Matrix3d>::const_iterator stress_it = stresses.begin();
     for (const Tetrahedron& tet : mesh->tets) {
         const uint32_t *verts = tet.vertices();
         for (int i = 0; i < 4; i++) {
@@ -81,7 +89,28 @@ step(Mesh *mesh, double dt)
             vert.f += *stress_it * norms;
         }
     }
+}
+// --------------------------------------------------------------------------
 
+void
+step(Mesh *mesh, double dt)
+{
+    assert(mesh != NULL);
+
+    std::vector<Tetrahedron>::const_iterator tet_it;
+    std::vector<Matrix3d>::iterator def_it;
+    std::vector<Matrix3d>::iterator strain_it;
+    std::vector<Matrix3d>::iterator stress_it;
+
+    // Clear forces
+    for (Vertex& v : mesh->verts) {
+        v.f = Eigen::Vector3d(0, 0, 0);
+    }
+
+    computeDeformations(mesh->tets, sDeformations);
+    computeStrains(sDeformations, sStrains);
+    computeStresses(sDeformations, sStrains, sStresses);
+    computeForces(sStresses, mesh);
 
     // Integrate
     for (Vertex& v : mesh->verts) {
@@ -121,3 +150,4 @@ main(int argc, char* argv[])
 
     return SUCCESS;
 }
+// --------------------------------------------------------------------------
